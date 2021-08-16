3b0046ee3618eb172355fe7b09e7cc1982496938
jf li <lijf1997@outlook.com>
2021-08-16 22:27:52 +0800

    publish=""

---
+++ src/com/study/test/TestDemo.java
@@ -0,0 +1,13 @@
+package com.study.test;
+
+/**
+ * com.study.test
+ * @Author: JF LI
+ * @Date: 2021/8/16 22:23
+ * @Version v1.0
+ **/
+public class TestDemo {
+    public static void main(String[] args){
+        System.out.println("Test");
+    }
+}
