13af00ae2c5f61c45eed8ec37ced38cb84d4002f
Jiafeng1.Li@lcfuturecenter.com <Jiafeng1.Li@lcfuturecenter.com>
2021-08-18 20:23:54 +0800

    publish=""

--- src/com/study/classdemo/Student.java
+++ src/com/study/classdemo/Student.java
@@ -5,13 +5,26 @@ public class Student {
     public String name;
     public int age;
     public String address;
-    public void name(){
+
+    public void name() {
         System.out.println("学生需要有姓名");
     }
-    public void age(){
+
+    public void age() {
         System.out.println("学生需要有姓名");
     }
-     public void address(){
+
+    public void address() {
         System.out.println("学生需要有地址");
-     }
+    }
+
+    public void getName(String name) {
+        System.out.println(name);
+        // 打印成员变量
+        System.out.println(this.name);
+    }
+
+    public void method() {
+        System.out.println("method方法的this:" + this);
+    }
 }
--- src/com/study/test/ClassTest.java
+++ src/com/study/test/ClassTest.java
@@ -4,12 +4,30 @@ import com.study.classdemo.Student;
 
 /** @author Jiafeng1.Li */
 public class ClassTest {
+
+    // private :只能在同一个类中访问;
+    // （default）：同一个类中，同一个包下访问;
+    // protected:;
+    // public :同一个包，同一个类，不同包;
+
     public static void main(String[] args) {
         Student stu = new Student();
+
+        stu.name = "李四";
         /*对象在内存中的地址值*/
         System.out.println(stu);
         stu.name();
         stu.age();
         stu.address();
+        // 就近原则，在局部变量和成员变量重名时，采用就近原则
+        stu.getName("我是张三");
+        /*
+            this代表当前类的对象引用（地址）
+            哪一个对象调用的this，this就代表哪一个对象，用于区分局部变量和成员变量的重名问题
+        * */
+        stu.method();
+        Student stu1 = new Student();
+        System.out.println(stu1);
+        stu1.method();
     }
 }
