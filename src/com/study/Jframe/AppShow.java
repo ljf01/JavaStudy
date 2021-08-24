7cd41d6e40c2413d3fd510ec6e3fc30f12d6a05d
jf li <lijf1997@outlook.com>
2021-08-24 22:25:24 +0800

    publish=""

--- src/com/study/Jframe/AppShow.java
+++ src/com/study/Jframe/AppShow.java
@@ -1,7 +1,6 @@
 package com.study.Jframe;
 
 import javax.swing.*;
-import java.net.URL;
 
 /** @author Jiafeng1.Li */
 public class AppShow {
@@ -11,7 +10,7 @@ public class AppShow {
         frame.setSize(514, 538);
         frame.setLocationRelativeTo(null);
         frame.setAlwaysOnTop(true);
-        frame.setDefaultCloseOperation(3);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         // 1.通过窗体对象，取消默认布局
         frame.setLayout(null);
         // 2.创建按钮对象
@@ -24,10 +23,9 @@ public class AppShow {
         frame.getContentPane().add(btn2);
 
         // JLabel展示文本和图片
-        URL url;
         ImageIcon icon =
                 new ImageIcon(
-                        "C:\\Users\\jiafeng1.li\\Pictures\\Saved Pictures\\a871d40f882111ebb6edd017c2d2eca2.jpg");
+                        "src/com/study/image/shitou/1.png");
         JLabel label = new JLabel(icon);
         label.setBounds(50, 150, 300, 300);
         frame.getContentPane().add(label);
