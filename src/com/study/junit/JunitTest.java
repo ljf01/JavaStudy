c319388c9dc7043f7defe80482ebd9f829edda11
jf li <lijf1997@outlook.com>
2021-09-06 21:58:45 +0800

    publish=""

--- src/com/study/junit/JunitTest.java
+++ src/com/study/junit/JunitTest.java
@@ -10,6 +10,7 @@ public class JunitTest {
     public void sub(int a, int b) {
         System.out.println(a - b);
     }
+
     /**
      * Junit使用： 1. 定义一个测试类 建议：测试类名：被测试类名加Test 包名：xxx.xxx.xx.test 2.定义测试方法：可以独立运行 建议：方法名，testXxx
      * 返回值：void 参数列表:空参 3.给方法加@Test注解 4.导入Junit依赖
