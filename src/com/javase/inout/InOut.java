5e0ab7e0a77b3894cc450e359646b4815ea878d1
Jiafeng1.Li@lcfuturecenter.com <Jiafeng1.Li@lcfuturecenter.com>
2021-10-26 20:24:35 +0800

    publish=""

---
+++ src/com/javase/inout/InOut.java
@@ -0,0 +1,28 @@
+package com.javase.inout;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/**
+ * @author Jiafeng1.Li
+ */
+public class InOut {
+    public static void main(String[] args)
+    {
+        try {
+            //读取文件
+            Scanner sc = new Scanner(Paths.get("D:\\javase\\javase.txt"), StandardCharsets.UTF_8);
+            String temp = sc.nextLine();
+            System.out.println(temp);
+            //写入文件
+            PrintWriter pr = new PrintWriter("D:\\javase\\Test.txt", StandardCharsets.UTF_8);
+            pr.print("TEST_TEST_TEST");
+            pr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
